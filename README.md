8745960340c932e4ff1c67e981691762885aef7d
ZTL-UwU <zhangtianli2006@163.com>
2023-03-30 14:48:12 +0800

    api: POST /check
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- README.md
+++ README.md
@@ -13,7 +13,12 @@ cargo run
 
 ## API
 
-`POST /add`: 
+- `POST /check`
+- `POST /add`
+
+`check` 为检查论文相似度
+
+`add` 为 `check` 之后将论文加入数据库 **!!暂不支持修改!!**
 
 **请求:**
 
@@ -24,7 +29,7 @@ cargo run
 }
 ```
 
-其中 id 应不与已有重复。
+其中 id 应不与已有论文重复。
 
 **响应:**
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use jieba_rs::Jieba;
 use rocket::serde::{json::Json, Deserialize, Serialize};
+use serde_json::Value;
 
 mod cut;
 mod data;
@@ -17,7 +18,7 @@ extern crate rocket;
 extern crate lazy_static;
 
 #[derive(Deserialize, Serialize, Debug)]
-struct AddData {
+struct ReqData {
     id: String,
     text: String,
 }
@@ -27,11 +28,7 @@ struct ResData {
     similarity: Vec<(f64, String)>,
 }
 
-#[post("/add", format = "json", data = "<data>")]
-fn add(data: Json<AddData>) -> Json<ResData> {
-    let req = data.into_inner();
-    let mut store = data::open_data();
-
+fn get_similarity(req: &ReqData, store: &mut Value) -> Vec<(f64, String)> {
     // Remove whitespace
     let trimmed = req.text.chars().filter(|c| !c.is_whitespace()).collect();
     // Cut text
@@ -40,11 +37,20 @@ fn add(data: Json<AddData>) -> Json<ResData> {
     let tf_array = process::get_tf_array(&sep_text);
 
     // Add paper
-    process::add_paper(req.id.clone(), &tf_array, &mut store);
+    process::add_paper(req.id.clone(), &tf_array, store);
     // Update df
-    process::update_feature_names(sep_text, &mut store);
+    process::update_feature_names(sep_text, store);
+
     // Get result
-    let res = process::get_global_similarity(req.id.clone(), &tf_array, &store);
+    process::get_global_similarity(req.id.clone(), &tf_array, store)
+}
+
+#[post("/add", format = "json", data = "<data>")]
+fn add(data: Json<ReqData>) -> Json<ResData> {
+    let mut store = data::open_data();
+    let req = data.into_inner();
+
+    let res = get_similarity(&req, &mut store);
 
     // Write data
     data::write_data(&store).unwrap();
@@ -52,8 +58,18 @@ fn add(data: Json<AddData>) -> Json<ResData> {
     Json(ResData { similarity: res })
 }
 
+#[post("/check", format = "json", data = "<data>")]
+fn check(data: Json<ReqData>) -> Json<ResData> {
+    let mut store = data::open_data();
+    let req = data.into_inner();
+
+    Json(ResData {
+        similarity: get_similarity(&req, &mut store),
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
     // Start Rocket server
-    rocket::build().mount("/", routes![add])
+    rocket::build().mount("/", routes![add, check])
 }
